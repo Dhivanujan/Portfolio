6237a5aaa975766578dae75e245bcbb9509e8e7d
Dhivanujan <mahadevvinayak837@gmail.com>
2025-11-02 21:19:20 +0530

    Styles added in index.css and themetoggle created

---
+++ MyPortfolio/src/components/ThemeToggle.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { Sun } from "lucide-react";
+import { Moon } from "lucide-react";
+
+export const ThemeToggle = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    return <button>{isDarkMode ? <Sun/> : <Moon/>}</button>
+};
